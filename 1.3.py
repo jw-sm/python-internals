5ce8113d61e2be8b3884fa5edf4416d47923c5fe
del3500 <wendellsmesia@gmail.com>
2025-08-07 01:46:53 +0800

    experiment w/ threading

---
+++ 1.3.py
@@ -0,0 +1,17 @@
+import threading
+
+
+def hello_from_thread():
+    print(f"hello from thread {threading.current_thread()}!")
+
+
+hello_thread = threading.Thread(target=hello_from_thread)
+hello_thread.start()
+
+total_threads = threading.active_count()
+thread_name = threading.current_thread().name
+
+print(f"python is currently running {total_threads} thread(s)")
+print(f"the current thread is {thread_name}")
+
+hello_thread.join()
