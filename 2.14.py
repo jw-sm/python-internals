b7c5a7178ee1ba37e35e83e1e2033601788ce67b
del3500 <wendellsmesia@gmail.com>
2025-08-10 04:33:56 +0800

    experimenting with Future module

--- 2.14.py
+++ 2.14.py
@@ -6,6 +6,5 @@ print(f"Is my_future done? {my_future.done()}")
 
 my_future.set_result(42)
 
-
 print(f"Is my_future done? {my_future.done()}")
 print(f"What is the result of my_future? {my_future.result()}")
